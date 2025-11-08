3c33be2f4712c67e6d2dd93aa3b9cd2628049481
sayeed <expsayeedshorif@gmail.com>
2025-11-08 20:38:04 +0600

    did the about me section and made it responsive too

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,13 +8,13 @@ const About = ({ refs }) => {
         target:refs,
         offset: ['start end', 'end start']
     })
-    const y = useTransform(scrollYProgress, [0, 1], [-200, 200])
+    const y = useTransform(scrollYProgress, [0, 1], [-300, 300])
     const opacity = useTransform(scrollYProgress, [0, 0.5, 1], [0, 1, 0])
 
     return (
 
-        <div ref={refs} className='min-h-screen overflow-hidden flex items-center justify-center w-[90vw] gap-x-10 mx-auto'>
-            <motion.div style={{y, opacity}} className='text-[#F8F4E3] font-fira w-[40%] flex flex-col gap-y-6'>
+        <div ref={refs} className='min-h-screen   flex items-center justify-center flex-col lg:flex-row w-[90vw] gap-x-10 mx-auto gap-y-20'>
+            <motion.div style={{y, opacity}} className='text-[#F8F4E3] font-fira w-full lg:w-[40%] flex flex-col gap-y-6'>
                 <div className=''>
                     <p className='uppercase'>Read More</p>
                     <p className='capitalize font-outfit text-5xl font-semibold'>About me</p>
@@ -22,7 +22,7 @@ const About = ({ refs }) => {
                 <p className='text-xl'>Hello, my name is Sayeed Shorif and I’m a self-taught front-end developer and designer. Tyson is the nickname I use across social medias. I build responsive, accessible websites from scratch that look and feel fresh and creative. I’m committed to developing products that focus on usability and simplicity.</p>
                 <p className='text-xl'>I love learning new things and technologies and I’m always gathering inspiration and exploring different ideas and techniques. I’m curious and passionate, especially when it comes to art, music and gaming.</p>
             </motion.div>
-            <div className='max-w-[30%] group transition-all duration-300 scale-100 hover:scale-105'>
+            <div className='lg:w-[40%] md:flex-1 xl:max-w-[30%] group transition-all duration-300 scale-100 hover:scale-105'>
                 <ElectricBorder
                     color="#7df9ff"
                     speed={1}
